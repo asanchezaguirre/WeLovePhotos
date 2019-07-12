85d093b737fab6949c90155638e1d9858ca96111
Adriana <s.aguirre.adriana@gmail.com>
2019-07-12 17:40:00 -0500

    indentacion

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,78 +13,73 @@ class Home extends Component {
 	}
 
 	handleInputChange = event => {
-    const query = event.target.value;
+    	const query = event.target.value;
+    	this.setState(prevState => {
+      		const filteredData = prevState.data.filter(element => {
+        	return element.name.toLowerCase().includes(query.toLowerCase()) || element.username.toLowerCase().includes(query.toLowerCase());
+      		});
+      	return {
+        	query,
+        	filteredData
+      	};
+    	});
+  	};
 
-    this.setState(prevState => {
-      const filteredData = prevState.data.filter(element => {
-        return element.name.toLowerCase().includes(query.toLowerCase()) || element.username.toLowerCase().includes(query.toLowerCase());
-      });
+  	getData = () => {
+    	fetch(`https://jsonplaceholder.typicode.com/users`)
+      		.then(response => response.json())
+      		.then(data => {
+        		const { query } = this.state;
+        		const filteredData = data.filter(element => {
+          			return element.name.toLowerCase().includes(query.toLowerCase());
+       			 });
+        		this.setState({
+          			data,
+          			filteredData
+        		})
+      		})
+     		.catch(e => alert(e))
+  	};
 
-      return {
-        query,
-        filteredData
-      };
-    });
-  };
+  	componentWillMount() {
+    	this.getData();
+  	}	
 
-  getData = () => {
-    fetch(`https://jsonplaceholder.typicode.com/users`)
-      .then(response => response.json())
-      .then(data => {
-        const { query } = this.state;
-        const filteredData = data.filter(element => {
-          return element.name.toLowerCase().includes(query.toLowerCase());
-        });
-
-        this.setState({
-          data,
-          filteredData
-        });
-      });
-     
-  };
-
-  componentWillMount() {
-    this.getData();
-  }
-
-	
-
-  render() {
-      return (
-      	<div className="container-fluid">
-	      	<div className="form-group row">
-	      		<label className="col-1 col-form-label offset-2">Search</label>
-    			<div className="col-7">
-      				<input 
-	      				   onChange={this.handleInputChange} 
-	      				   type="search" 
-	      				   aria-label="Search" 
-	      				   className="form-control" 
-	      			/>
-    			</div>
-	      	</div>
-	      	{this.state.filteredData.map((user, index) => (
-	      	<div className="row" key={index}>
-	      		<div className="card  border-dark col-8 offset-2">
-	      			<div className="row align-items-center">
-						<div className="card_home col-1">
-						  <img src={`https://api.adorable.io/avatars/${user.name}`} className="card-img" alt="User avatar"/>
-						</div>  
-						<div className="col-8">
-						   <h5 className="card-title">@{user.username}</h5>
-						   <p className="card-text">{user.name}</p>
+  	render() {
+      	return (
+      		<div className="container-fluid">
+	      		<div className="form-group row">
+	      			<label className="col-1 col-form-label offset-2">Search</label>
+	    			<div className="col-7">
+	      			<input 
+		      			onChange={this.handleInputChange} 
+		      			type="search" 
+		      			aria-label="Search" 
+		      			className="form-control" 
+		      		/>
+	    			</div>
+	      		</div>
+	      		{this.state.filteredData.map((user, index) => (
+		      	<div className="row" key={index}>
+		      		<div className="card  border-dark col-8 offset-2">
+		      			<div className="row align-items-center">
+							<div className="card_home col-1">
+							  <img src={`https://api.adorable.io/avatars/${user.name}`} className="card-img" alt="User avatar"/>
+							</div>  
+							<div className="col-8">
+							   <h5 className="card-title">@{user.username}</h5>
+							   <p className="card-text">{user.name}</p>
+							</div>
+							<div className="card-body col-3">
+							   <Link to={`/users/${user.id}`} className="btn btn-outline-dark">See Albums</Link>
+							</div>
 						</div>
-						<div className="card-body col-3">
-						   <Link to={`/users/${user.id}`} className="btn btn-outline-dark">See Albums</Link>
-						</div>
-					</div>
-		    	</div>
-	      	</div>
- 			))}
-		</div>
-        );
-  }
+			    	</div>
+		      	</div>
+	 			))}
+			</div>
+	    );
+  	}
 }
 
 export default Home;
--- src/components/User.js
+++ src/components/User.js
@@ -8,15 +8,13 @@ class User extends Component {
 		this.state={
 			albumes:[],
 			user:{},
-		}
-		
+		}		
 	}
 
 	componentDidMount(){
 		const userId = this.props.match.params.userId;
 		this.showAlbumes(userId);
 		this.showCurrentUser(userId);
-
 	}
 	
 	showCurrentUser = userId => {
@@ -24,15 +22,14 @@ class User extends Component {
 	    fetch(`${API_URL}${this.props.match.params.userId}`, {
 	      method: 'GET',
 	    })
-	      .then(response => response.json())
-	      .then(data => {
+	      	.then(response => response.json())
+	      	.then(data => {
 	        //console.log(data)
-	        this.setState({
-	          user: data
-	        })
-
-	      })
-	      .catch(e => alert(e))
+		        this.setState({
+		          	user: data
+		        })
+		    })
+	      	.catch(e => alert(e))
 	}	
 
 	showAlbumes = userId => {
@@ -40,64 +37,49 @@ class User extends Component {
 	    fetch(`${API_URL}${this.props.match.params.userId}`, {
 	      method: 'GET',
 	    })
-	      .then(response => response.json())
-	      .then(data => {
+	      	.then(response => response.json())
+	      	.then(data => {
 	        //console.log(data)
-	        this.setState({
-	          albumes: data
-	        })
+	        	this.setState({
+	          		albumes: data
+	        	})
+	      	})
+	      	.catch(e => alert(e))
+  	}
 
-	      })
-	      .catch(e => alert(e))
-  }
 
-
-  render() {
+  	render() {
   	    return (
-      	<div className="container"> 
-      		
-      				<div className="row align-items-center mb-2">
-      			
-
-			      		<div className="user_avatar">
-				      		<img src={`https://api.adorable.io/avatars/${this.state.user.name}`} alt="User avatar"/>
-				      	</div> 
-				      	<div className="col">
-			      			<h5 className="font-weight-bold">@{this.state.user.username}</h5>
-			      			<p>{this.state.user.name}</p>
-			      		</div>
-		
-			</div>
-
-
-	      	<div className="row">    
-	      		
-	      			<h4 className="font-weight-bold">Albums</h4>
-	      				
-	      	</div>
-	      	<div className="row">
-		      	
-			      	{this.state.albumes.map((album, index) => (
-			      		
-			      			<div className="col-3" key={index}>
-				      			<div>
-				      				<Link to={`/${album.userId}/${album.id}`}>
-										<div className="card card_album p-2 mb-3">
-											<img src={`https://picsum.photos/id/${album.id}/400`} className="card-img-top" alt="Album de usuario"/>
-										
-											<p className="text-decoration-none text-justify">{album.title}</p>
-										
-										</div>
-									</Link>	
+      		<div className="container">      		
+      			<div className="row align-items-center mb-2">
+			      	<div className="user_avatar">
+				      	<img src={`https://api.adorable.io/avatars/${this.state.user.name}`} alt="User avatar"/>
+				    </div> 
+				    <div className="col">
+			      		<h5 className="font-weight-bold">@{this.state.user.username}</h5>
+			      		<p>{this.state.user.name}</p>
+			      	</div>	
+				</div>
+	      		<div className="row">        		
+	      			<h4 className="font-weight-bold">Albums</h4>				
+		      	</div>
+		      	<div className="row">			      	
+				    {this.state.albumes.map((album, index) => (				      		
+				    <div className="col-3" key={index}>
+					    <div>
+					      	<Link to={`/${album.userId}/${album.id}`}>
+								<div className="card card_album p-2 mb-3">
+									<img src={`https://picsum.photos/id/${album.id}/400`} className="card-img-top" alt="Album de usuario"/>											
+									<p className="text-decoration-none text-justify">{album.title}</p>											
 								</div>
-							</div>
-						
-		 			))}
-		 		
- 			</div>
-		</div>
+							</Link>	
+						</div>
+					</div>						
+		 			))}		 		
+ 				</div>
+			</div>
         );
-  }
+  	}
 }
 
 export default User;
